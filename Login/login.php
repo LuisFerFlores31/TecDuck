b854d8477554d9cd29f06666ca0546e5a6ddc273
LuisFerFlores31 <a01738347@tec.mx>
2025-05-02 10:07:08 -0600

    MultipleQ working with local DB

---
+++ Login/login.php
@@ -0,0 +1,25 @@
+<?php
+
+$dbhost = 'localhost';
+$dbuser = 'root';
+$dbpass = '';
+$dbname = 'test';
+
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$nombre = $_POST["username"];
+$pass = $_POST["password"];
+
+$query = mysqli_query($conn, "SELECT * FROM login WHERE username='$nombre' AND password='$pass'");
+$nr = mysqli_num_rows($query);
+
+if ($nr == 1) {
+    echo "Login successful ". $nombre;
+} else {
+    echo "Invalid username or password";
+}
+
+?>
